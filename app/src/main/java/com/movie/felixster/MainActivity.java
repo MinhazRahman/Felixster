16b93716374de5f57cd178f992959bff0a410478
Mohammed Rahman <minhaz357@gmail.com>
2022-02-02 18:18:11 -0500

    Send a GET request to the Movie Database API

--- app/src/main/java/com/movie/felixster/MainActivity.java
+++ app/src/main/java/com/movie/felixster/MainActivity.java
@@ -1,14 +1,39 @@
 package com.movie.felixster;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL_NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // sending a GET request to the Movie Database API
+        // create an AsyncHttpClient, and
+        // then execute a request specifying an anonymous class as a callback
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        asyncHttpClient.get(URL_NOW_PLAYING, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
     }
 }
