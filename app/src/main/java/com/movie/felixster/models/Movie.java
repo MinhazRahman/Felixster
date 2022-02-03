e1fd799ea3d348cfff27133fcf98f00ea7a8a636
Mohammed Rahman <minhaz357@gmail.com>
2022-02-02 23:11:22 -0500

    Create the basic model class Movie

---
+++ app/src/main/java/com/movie/felixster/models/Movie.java
@@ -0,0 +1,47 @@
+package com.movie.felixster.models;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String title;
+    String posterPath;
+    String overview;
+
+    // construct the Movie object
+    public Movie(@NonNull JSONObject jsonObject) throws JSONException {
+        title = jsonObject.getString("title");
+        posterPath = jsonObject.getString("poster_path");
+        overview = jsonObject.getString("overview");
+    }
+
+    // get the List of Movies from the json array
+    @NonNull
+    public static List<Movie> getMovies(@NonNull JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i=0; i<movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
