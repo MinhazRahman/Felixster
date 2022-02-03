5a048337b102d12a8202afd63dce571f7cad5448
Mohammed Rahman <minhaz357@gmail.com>
2022-02-02 23:24:49 -0500

    Print the list of movies to the console

--- app/src/main/java/com/movie/felixster/MainActivity.java
+++ app/src/main/java/com/movie/felixster/MainActivity.java
@@ -6,10 +6,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.movie.felixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String URL_NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
+                    movies = Movie.getListOfMovies(results);
+
                     Log.i(TAG, "Results: " + results);
+                    Log.i(TAG, String.valueOf(movies));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
